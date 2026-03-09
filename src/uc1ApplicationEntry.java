5995478ae466bac4278f6f1b2575bd1d674c9164
AkshitaM21 <akshitam2110@gmail.com>
2026-03-09 09:49:17 +0530

    UC1: Application entry implemented

---
+++ src/uc1ApplicationEntry.java
@@ -0,0 +1,12 @@
+public class uc1ApplicationEntry {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("--------------------------------");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+
+    }
+}
