5ff7511f096c3e4299577472a21a1f75aa50ee6e
AkshitaM21 <akshitam2110@gmail.com>
2026-03-15 18:09:48 +0530

    Add inventory setup required for UC4

---
+++ src/uc3InventorySetup.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+
+        inventory.put("Single Room", 10);
+        inventory.put("Double Room", 5);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int newCount) {
+        inventory.put(roomType, newCount);
+    }
+
+    public void displayInventory() {
+        System.out.println("===== Current Room Inventory =====");
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
+
+public class uc3InventorySetup {
+
+    public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        inventory.displayInventory();
+
+        System.out.println("\nUpdating availability...\n");
+
+        inventory.updateAvailability("Single Room", 8);
+
+        inventory.displayInventory();
+    }
+}
